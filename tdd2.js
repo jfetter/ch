9322b90ff3b96e607318388e4f4035aa55490d71
Jillian Fetter <jillian.fetter@gmail.com>
2015-10-29 15:35:39 -0700

    pulled some Samer

---
+++ tdd2.js
@@ -0,0 +1,8 @@
+var longestString =  function(){
+	//return the longest string
+}
+
+
+console.assert(longestString("abc","abbb","donkey"), "donkey");
+console.assert(longestString(1,[3,4,5,6,7],"donkey", "hundred"), "hundred");
+console.assert(longestString("1",{one: "two", two: "three"},"money"), "money");
